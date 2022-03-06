b8fe163d144f472dcdb0e24090509ee5bb6576a4
Aarush Gupta <aarush@theaarushgupta.com>
2022-03-06 10:54:27 -0800

    feat: modify Gulpfile to ship shaders as well

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,14 +6,25 @@ var buffer = require("vinyl-buffer");
 
 const project = ts.createProject("tsconfig.json");
 
+const RESOURCES = "./res/**/*";
 const DESTINATION = "./bin";
 
-gulp.task("default", function () {
+gulp.task("copy-res", () => {
+    return gulp.src(RESOURCES).pipe(gulp.dest(DESTINATION + "/res"))
+})
+
+gulp.task("build-ts", () => {
     return project.src()
-        .pipe( project() ).js
-        .pipe( buffer() )
-        .pipe( sourcemaps.init({ loadMaps: true }) )
-        .pipe( uglify() )
-        .pipe( sourcemaps.write("./") )
-        .pipe( gulp.dest(DESTINATION) )
+        .pipe(project()).js
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest(DESTINATION));
 });
+
+gulp.task(
+    "default",
+    gulp.series(gulp.parallel("copy-res")),
+    gulp.series(gulp.parallel("build-ts"))
+);
