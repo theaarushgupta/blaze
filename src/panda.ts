b359ea4e69f0f5d1f82268a9209d65a88b9238c9
Aarush Gupta <aarush@theaarushgupta.com>
2022-03-06 14:39:49 -0800

    feat: add null-safety to initialization

--- src/panda.ts
+++ src/panda.ts
@@ -10,14 +10,18 @@ class Panda {
         this.state = {
             gl: app.getContext("webgl2")
         };
-        this.shader = new shader.Shader(this.state);
         this.setup();
+    }
+
+    clear(): void {
+        this.state.gl.clearColor(0.0, 0.8, 0.0, 1.0); // green
+        this.state.gl.clear(this.state.gl.COLOR_BUFFER_BIT);
     }
 
     setup(): void {
         if ( this.state.gl == null ) { return; }
-        this.state.gl.clearColor(0.0, 0.8, 0.0, 1.0); // green
-        this.state.gl.clear(this.state.gl.COLOR_BUFFER_BIT);
+        this.clear();
+        this.shader = new shader.Shader(this.state);
     }
 };
 
